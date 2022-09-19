f2a7d21b85472b86a23e1a106b11236e6aab58f3
Erival Junior <sanjunior@gmail.com>
2022-09-19 20:08:34 -0300

    update migrations

--- erijob/settings.py
+++ erijob/settings.py
@@ -140,9 +140,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR.parent.parent / "static"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = "static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "erijob/static",
+]
 
 
 # Default primary key field type

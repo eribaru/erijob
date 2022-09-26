7c2c523ad8aade85c7db9d2b849a74a8e02abc3b
Erival Junior <sanjunior@gmail.com>
2022-09-26 12:24:51 -0300

    adição swagger

--- erijob/urls.py
+++ erijob/urls.py
@@ -4,8 +4,20 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import routers
 from knox import views as knox_views
 from erijob.apps.api.views import LoginView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
 
 # Serializers define the API representation.
+
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="Posts API",
+        default_version='1.0.0',
+        description="API documentation of App",
+    ),
+    public=True,
+)
 
 # Routers provide a way of automatically determining the URL conf.
 router = routers.DefaultRouter()
@@ -13,7 +25,10 @@ router = routers.DefaultRouter()
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api/v1/', include('erijob.apps.api.urls', namespace='erijob.apps.api')),
+    path('api/v1/',  include([
+            path('', include('erijob.apps.api.urls', namespace='erijob.apps.api')),
+            path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema"),
+        ])),
     path(r'api/auth/', include('knox.urls')),
     path('admin/', admin.site.urls),
     path(r'login/', csrf_exempt(LoginView.as_view())),
